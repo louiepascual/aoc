ec7c29bac1782d7c8486e456343a7ae3f2662d9f
Louie Pascual <louieepascual@gmail.com>
2024-01-16 00:40:14 +0800

    Change tab to spaces

--- one/main.py
+++ one/main.py
@@ -2,41 +2,41 @@
 # O(n^2)
 
 str_digits = {
-	"one": 1,
-	"two": 2,
-	"three": 3,
-	"four": 4,
-	"five": 5,
-	"six": 6,
-	"seven": 7,
-	"eight": 8,
-	"nine": 9
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9
 }
 
 def main():
-	with open("input.txt") as f:
-		lines = f.readlines()
-		
-	total_value = 0
-	for i in lines:
-		line = i.strip()
-		
-		nums = []
-		itr = 0
-		for j in line:
-			if j.isdigit():
-				nums.append(int(j))
-			else:
-				for k in str_digits:
-					if line[itr:itr+(len(k))] == k:
-						nums.append(str_digits[k])
-						break
-			itr = itr + 1
+    with open("input.txt") as f:
+        lines = f.readlines()
+        
+    total_value = 0
+    for i in lines:
+        line = i.strip()
+        
+        nums = []
+        itr = 0
+        for j in line:
+            if j.isdigit():
+                nums.append(int(j))
+            else:
+                for k in str_digits:
+                    if line[itr:itr+(len(k))] == k:
+                        nums.append(str_digits[k])
+                        break
+            itr = itr + 1
 
-		# print(nums)
-		number = (nums[0] * 10) + nums[-1]
-		total_value += number
+        # print(nums)
+        number = (nums[0] * 10) + nums[-1]
+        total_value += number
 
-	print(total_value)
+    print(total_value)
 
 main()
